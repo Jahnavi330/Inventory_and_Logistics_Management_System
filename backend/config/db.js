c2ee1478ee811b3f1a19ba9fcea907a9a79f9440
Jahna Neyyala <jahnavineyyala07@gmail.com>
2026-07-02 17:21:28 +0530

    changed db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 
-const mysql = require("mysql2");
-
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
+    connectTimeout: 60000,
     multipleStatements: true
 });
 
